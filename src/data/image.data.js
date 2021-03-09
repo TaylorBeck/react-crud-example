9d9c8654f1bfae2f7c112354c09f73d8a0ab24a8
Taylor Beck <beck.taylorg@gmail.com>
2021-03-09 11:12:01 -0800

    Add placeholder imageUrls

---
+++ src/data/image.data.js
@@ -0,0 +1,14 @@
+const imageUrls = [
+  'https://i.ibb.co/YfGd1H9/image.jpg',
+  'https://i.ibb.co/JqLv3wf/image-2.jpg',
+  'https://i.ibb.co/zRrzcMz/image-3.jpg',
+  'https://i.ibb.co/yymNkbw/image-4.jpg',
+  'https://i.ibb.co/cCNHmD0/image-5.jpg',
+  'https://i.ibb.co/Lxzg4kj/image-6.jpg',
+  'https://i.ibb.co/4P4RXN1/image-7.jpg',
+  'https://i.ibb.co/jVRcdY5/image-8.jpg',
+  'https://i.ibb.co/J33HFdv/image-9.jpg',
+  'https://i.ibb.co/jg1RNk6/image-10.jpg'
+];
+
+export default imageUrls;
