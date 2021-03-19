cca80c9c94284aecdb3ee2393cbf3a5617fa4769
Taylor Beck <beck.taylorg@gmail.com>
2021-03-18 22:21:51 -0700

    Optimize user profile images

--- src/data/image.data.js
+++ src/data/image.data.js
@@ -1,14 +1,14 @@
 const imageUrls = [
-  'https://i.ibb.co/YfGd1H9/image.jpg',
-  'https://i.ibb.co/JqLv3wf/image-2.jpg',
-  'https://i.ibb.co/zRrzcMz/image-3.jpg',
-  'https://i.ibb.co/yymNkbw/image-4.jpg',
-  'https://i.ibb.co/cCNHmD0/image-5.jpg',
-  'https://i.ibb.co/Lxzg4kj/image-6.jpg',
-  'https://i.ibb.co/4P4RXN1/image-7.jpg',
-  'https://i.ibb.co/jVRcdY5/image-8.jpg',
-  'https://i.ibb.co/J33HFdv/image-9.jpg',
-  'https://i.ibb.co/jg1RNk6/image-10.jpg'
+  'https://i.ibb.co/DW9Cr7j/image-1.jpg',
+  'https://i.ibb.co/gdFgKPS/image-2.jpg',
+  'https://i.ibb.co/4dDtkVM/image-3.jpg',
+  'https://i.ibb.co/mvSxGzD/image-4.jpg',
+  'https://i.ibb.co/cvdsvnv/image-5.jpg',
+  'https://i.ibb.co/XDPyBBx/image-6.jpg',
+  'https://i.ibb.co/mJ1x5d5/image-7.jpg',
+  'https://i.ibb.co/ThWymsZ/image-8.jpg',
+  'https://i.ibb.co/CMYfG1K/image-9.jpg',
+  'https://i.ibb.co/9hW8w7f/image-10.jpg'
 ];
 
 export default imageUrls;
